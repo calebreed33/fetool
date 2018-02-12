0cfe2be9570a64465b6222ae10129a59c1cc536b
Caleb Reed <creed85@liberty.edu>
2018-02-12 00:39:00 -0500

    Toolbar added

--- FeTool/MainWindow.xaml.cs
+++ FeTool/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FeTool
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
