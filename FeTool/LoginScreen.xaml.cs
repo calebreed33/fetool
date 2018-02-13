e36fe8b9ba9936bcc7ebdda558edd0f660b38f50
Allison Patton <apatton1@liberty.edu>
2018-02-13 15:17:06 -0500

    Login Screen

---
+++ FeTool/LoginScreen.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FeTool
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        ComboBox comboBox1 = new ComboBox();
+        
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            comboBox1.Name = "Combobox1";
+            comboBox1.Background = SystemColors.White;
+            comboBox1.Items.Add("test1");
+
+
+        }
+    }
+}
