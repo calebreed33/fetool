ee6af2a3d1e9a44f2d304777e1c37f8a4bfa91eb
Caleb Reed <creed85@liberty.edu>
2018-02-19 14:20:22 -0500

    Replaced Expander with collapsing expander, Added converters folder, Added viewmodels folder, Fixed grid layout, Minor Fixes

---
+++ FeTool/ViewModels/ExpanderListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeTool.ViewModels
+{
+    public class ExpanderListViewModel
+    {
+        public Object SelectedExpander { get; set; }
+    }
+}
