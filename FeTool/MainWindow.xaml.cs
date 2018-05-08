594cc8925ba9ea62df0bc2fa49f72385b7b3204b
bainejamin <baineb3661@yahoo.com>
2018-05-08 03:02:41 -0400

    Fix Baseline Import

--- FeTool/MainWindow.xaml.cs
+++ FeTool/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace FeTool
         {
             InitializeComponent();
             this.DataContext = new MainWindowVM();
-            
+
             StackPanel1.DataContext = new ExpanderListViewModel();
             Generate_Users();
         }
@@ -79,7 +79,6 @@ namespace FeTool
                             reader.Close();
                         }
                         commentText.Clear();
-
                     }
                     sqlite_connection.Close();
                 }
@@ -91,8 +90,8 @@ namespace FeTool
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
             {
                 // Set filter for file extension and default file extension
-                DefaultExt = ".xls",
-                Filter = "Baseline Spreadsheet (*.xls)|*.xls"
+                DefaultExt = ".xls;.xlsx",
+                Filter = "Baseline Spreadsheet|*.xls;*.xlsx"
             };
 
             // Display OpenFileDialog by calling ShowDialog method
@@ -126,28 +125,41 @@ namespace FeTool
                 //This skips the header line
                 int i = 1;
 
-                //while (dataTable.Rows[0][0] <= dataTable.GetLength(1)) <--Remove if code works
-                //While there are unread rows in dataTable, import data from each row
-                while (i <= dataTable.Select().Length)
+                foreach (string database in globalvariables.DatabaseLocations)
                 {
-                    string systemName = dataTable.Rows[i][0].ToString();
-                    string checklist = dataTable.Rows[i][1].ToString();
-                    string topic = dataTable.Rows[i][2].ToString();
-                    string pdi = dataTable.Rows[i][3].ToString();
-                    string vKey = dataTable.Rows[i][4].ToString();
-                    string cat = dataTable.Rows[i][5].ToString();
-                    string discussion = dataTable.Rows[i][6].ToString();
-                    string notes = dataTable.Rows[i][0].ToString();
-                    string recommendation = dataTable.Rows[i][8].ToString();
-                    string iaControl = dataTable.Rows[i][9].ToString();
-                    string status = dataTable.Rows[i][10].ToString();
-                    DateTime dtNow = DateTime.Now;
-                    long dateTime = dtNow.Year * 10000000000 + dtNow.Month * 100000000 + dtNow.Day * 1000000 + dtNow.Hour * 10000 + dtNow.Minute * 100 + dtNow.Second;
-
-                    foreach (string database in globalvariables.DatabaseLocations)
+                    using (SQLiteConnection connection = new SQLiteConnection("Data Source=" + database + ";Version=3;"))
                     {
-                        using (SQLiteConnection connection = new SQLiteConnection("Data Source=" + database + ";Version=3;"))
+                        connection.Open();
+
+                        DateTime dtNow = DateTime.Now;
+                        long dateTime = dtNow.Year * 10000000000 + dtNow.Month * 100000000 + dtNow.Day * 1000000 + dtNow.Hour * 10000 + dtNow.Minute * 100 + dtNow.Second;
+
+                        //Add to Transactions
+                        SQLiteCommand command = new SQLiteCommand("INSERT INTO Transactions(transactionDateTime, userID)" +
+                            "VALUES (" + dateTime + ", " + globalvariables.SessionUser +")", connection);
+                        command.ExecuteNonQuery();
+                        long transactionID = connection.LastInsertRowId;
+
+                        //Add to DataSets
+                        command = new SQLiteCommand("INSERT INTO DataSets(dataSetType,transactionID)" +
+                            "VALUES (" + "'Baseline', " + transactionID + ")", connection);
+                        command.ExecuteNonQuery();
+
+                        //While there are unread rows in dataTable, import data from each row
+                        while (i <= dataTable.Select().Length)
                         {
+                            string systemName = dataTable.Rows[i][0].ToString();
+                            string checklist = dataTable.Rows[i][1].ToString();
+                            string topic = dataTable.Rows[i][2].ToString();
+                            string pdi = dataTable.Rows[i][3].ToString();
+                            string vKey = dataTable.Rows[i][4].ToString();
+                            string cat = dataTable.Rows[i][5].ToString();
+                            string discussion = dataTable.Rows[i][6].ToString();
+                            string notes = dataTable.Rows[i][0].ToString();
+                            string recommendation = dataTable.Rows[i][8].ToString();
+                            string iaControl = dataTable.Rows[i][9].ToString();
+                            string status = dataTable.Rows[i][10].ToString();
+
 //Use this code when you switch to a distributed DB, i.e. you're using separate tables for systems, STIGs, etc.
 /*
                             //if SysName not in DB.Systems, add it
@@ -184,16 +196,6 @@ namespace FeTool
                             tempreader.Read();
                             int stigID = (int)tempreader["Stig_ID"];
     */
-                            //Add to Transactions
-                            SQLiteCommand command = new SQLiteCommand("INSERT INTO Transactions(transactionDateTime, userID)" +
-                                "VALUES (" + dateTime + ", " + globalvariables.SessionUser +")", connection);
-                            command.ExecuteNonQuery();
-                            long transactionID = connection.LastInsertRowId;
-
-                            //Add to DataSets
-                            command = new SQLiteCommand("INSERT INTO DataSets(dataSetType,transactionID)" +
-                                "VALUES (" + "'Baseline', " + transactionID + ")", connection);
-                            command.ExecuteNonQuery();
 
                             //Add to ComplianceEntries
                             command = new SQLiteCommand("INSERT INTO ComplianceEntries(System_Name,Topic,PDI,V_Key," +
@@ -203,15 +205,22 @@ namespace FeTool
                                 ")", connection);
                             command.ExecuteNonQuery();
                             command.Dispose();
+                            }
+                            i++;
                         }
                     }
-                    i++;
-                }
+
                 reader.Close();
 
-                //TODO: Import baseline at baseline variable to database
                 string messageBoxText = "Imported " + baseline;
                 string caption = "Baseline Imported";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBox.Show(messageBoxText, caption, button, icon);
+            }
+            else{
+                string messageBoxText = "Import Failed: Invalid File";
+                string caption = "Import Failed";
                 MessageBoxButton button = MessageBoxButton.OK;
                 MessageBoxImage icon = MessageBoxImage.Information;
                 MessageBox.Show(messageBoxText, caption, button, icon);
